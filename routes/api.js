d2cc219948ba7d45e74742121ff26d7467acc991
Grant Pardington <grant.pardington@gmail.com>
2020-10-25 17:49:19 -0600

    Fixing Heroku DB connection

--- routes/api.js
+++ routes/api.js
@@ -21,15 +21,20 @@ router.post("/api/workouts", (req, res) => {
         });
 });
 
-router.put("/api/workouts/:id", function ({ body, params }, res) {
-    db.Workout.updateOne({ _id: params.id }, { $push: { exercises: body } })
-        .then((dbWorkout) => {
-            res.json(dbWorkout);
-        })
-        .catch((err) => {
-            res.json(err);
-        });
-});
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      // "runValidators" will ensure new exercises meet our schema requirements
+      { new: true, runValidators: true }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
